120c98c7b66e50418af12d98a16c11b2273bc6c1
Beau <casuist@icloud.com>
2017-08-05 13:27:55 +1000

    Initial Commit

---
+++ Checkbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checkbox
+//
+//  Created by Beau Nouvelle on 5/8/17.
+//  Copyright © 2017 Beau Nouvelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
