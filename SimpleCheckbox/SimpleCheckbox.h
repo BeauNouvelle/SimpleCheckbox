e059cb0b79940e72a1d47a95591cb8b6f7dfea3e
Beau Nouvelle <beaunouvelle@icloud.com>
2018-05-30 16:41:04 +1000

    Added Carthage Support

---
+++ SimpleCheckbox/SimpleCheckbox.h
@@ -0,0 +1,19 @@
+//
+//  SimpleCheckbox.h
+//  SimpleCheckbox
+//
+//  Created by Beau Nouvelle on 30/5/18.
+//  Copyright © 2018 Beau Nouvelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleCheckbox.
+FOUNDATION_EXPORT double SimpleCheckboxVersionNumber;
+
+//! Project version string for SimpleCheckbox.
+FOUNDATION_EXPORT const unsigned char SimpleCheckboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleCheckbox/PublicHeader.h>
+
+
--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SimpleCheckbox
 
 class ViewController: UIViewController {
 
